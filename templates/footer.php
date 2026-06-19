87ecf1273a31bc409ee1b8ba8fbaebb0cdc4cd3d
SergeyGrinn <sgrinkevich82@gmail.com>
2026-06-19 06:15:25 +0300

    Fixed footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <?php
 require_once base_path('templates/components/cartSidebar.php'); 
 ?>
-
+</main>
 <footer class="border-t px-8 py-5 text-center text-sm" style="background-color: #cbe3c5; border-color: #6a8a63; color: #888;">
         <p>&copy; <?= date('Y') ?> Server Catalog. All rights reserved.</p>
     </footer>
